952e550fbe053d44193ccc4c31b389540f698f31
Riccardo Giangiulio <riccardogiangi@gmail.com>
2025-04-13 17:35:35 +0200

    first commit

---
+++ src/main/java/com/riccardo/giangiulio/gestionescuola/GestioneScuolaApplication.java
@@ -0,0 +1,13 @@
+package com.riccardo.giangiulio.gestionescuola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneScuolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneScuolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riccardo/giangiulio/gestionescuola/GestioneScuolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riccardo.giangiulio.gestionescuola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneScuolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
