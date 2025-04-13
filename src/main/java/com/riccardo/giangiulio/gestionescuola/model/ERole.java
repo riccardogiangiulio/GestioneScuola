72b5e37ee0cbc58d7d25fa01dadf895688049383
Riccardo Giangiulio <riccardogiangi@gmail.com>
2025-04-13 18:30:41 +0200

    implementation for user and type of user

---
+++ src/main/java/com/riccardo/giangiulio/gestionescuola/model/ERole.java
@@ -0,0 +1,7 @@
+package com.riccardo.giangiulio.gestionescuola.model;
+
+public enum ERole {
+    ROLE_STUDENT,
+    ROLE_TEACHER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/riccardo/giangiulio/gestionescuola/model/User.java
@@ -0,0 +1,68 @@
+package com.riccardo.giangiulio.gestionescuola.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "The first name cannot be empty")
+    private String firstName;
+
+    @NotBlank(message = "The last name cannot be empty")
+    private String lastName;
+
+    @Email(message = "The email is not valid")
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @NotBlank(message = "The password cannot be empty")
+    private String password;
+
+    @Past(message = "The birth date cannot be in the future")
+    private LocalDate birthDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+    
+    public User(String firstName, String lastName, String email, String password, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+    }
+
+    public User(String firstName, String lastName, String email, String password, LocalDate birthDate, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.role = role; 
+    }
+}
